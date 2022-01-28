19d58d2940b7f8ea34b93ac7a63d498fecd69f4a
ITsverkunov <tsverkunovivan@gmail.com>
2022-01-28 13:47:35 +0300

    feat: add cards with js

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,49 @@ let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 const editButton = document.querySelector('.profile__edit-button');
 
+const cardTemplate = document.querySelector('#card-template').content;
+const cards = document.querySelector('.cards');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsArray = []
+
+function cloneCards () {
+  for (let i = 0; i < initialCards.length; i++) {
+    cardsArray.push(cardTemplate.querySelector('.card').cloneNode(true));
+    cardsArray[i].querySelector('.card__image').src = initialCards[i].link;
+    cardsArray[i].querySelector('.card__title').textContent = initialCards[i].name;
+    cards.append(cardsArray[i])
+  }
+}
+cloneCards();
+
+
 function saveInput () {
   nameInput.value = profileName.textContent;
   aboutInput.value = profileAbout.textContent;
